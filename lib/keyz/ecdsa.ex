e93e6f0b93e6ae5b682e395747dd048f6f72e454
mingshun <gutspot@qq.com>
2016-10-16 17:57:18 +0800

    Implements resolving public key from ECDSA key pem

--- lib/keyz/ecdsa.ex
+++ lib/keyz/ecdsa.ex
@@ -8,13 +8,26 @@ defmodule Keyz.ECDSA do
   """
   def generate curve_id do
     {public_key, private_key} = :crypto.generate_key :ecdh, curve_id
-    params_entity = {:namedCurve, :pubkey_cert_records.namedCurves(curve_id)}
-    key_entity = {:ECPrivateKey, 1, private_key, params_entity, public_key}
-    params_der_encoded = :public_key.der_encode :EcpkParameters, params_entity
-    key_der_encoded = :public_key.der_encode :ECPrivateKey, key_entity
+    ec_params = {:namedCurve, :pubkey_cert_records.namedCurves(curve_id)}
+    ec_private_key = {:ECPrivateKey, 1, private_key, ec_params, public_key}
+    ec_params_der = :public_key.der_encode :EcpkParameters, ec_params
+    ec_private_key_der = :public_key.der_encode :ECPrivateKey, ec_private_key
     :public_key.pem_encode [
-      {:EcpkParameters, params_der_encoded, :not_encrypted},
-      {:ECPrivateKey, key_der_encoded, :not_encrypted}
+      {:EcpkParameters, ec_params_der, :not_encrypted},
+      {:ECPrivateKey, ec_private_key_der, :not_encrypted}
     ]
+  end
+
+  @doc """
+  Returns public key in PEM binary corresponding to the given ECDSA key PEM binary.
+  """
+  def public_key pem do
+    [ec_params_pem, ec_private_key_pem] = :public_key.pem_decode pem
+    ec_params = :public_key.pem_entry_decode ec_params_pem
+    ec_private_key = :public_key.pem_entry_decode ec_private_key_pem
+    ec_point = {:ECPoint, ec_private_key |> elem(4)}
+    ec_public_key = {ec_point, ec_params}
+    spki_encoded = :public_key.pem_entry_encode :SubjectPublicKeyInfo, ec_public_key
+    :public_key.pem_encode [spki_encoded]
   end
 end
--- test/keyz/ecdsa_test.exs
+++ test/keyz/ecdsa_test.exs
@@ -3,11 +3,32 @@ defmodule Keyz.ECDSATest do
 
   alias Keyz.ECDSA
 
+  ### openssl ecparam -out ecdsa.pem -name secp256k1 -genkey
+  @test_pem """
+  -----BEGIN EC PARAMETERS-----
+  BgUrgQQACg==
+  -----END EC PARAMETERS-----
+  -----BEGIN EC PRIVATE KEY-----
+  MHQCAQEEIIa5Qh2LojbXXiEGWKukoc2CAx/1MJpjtib+QbLxyZW0oAcGBSuBBAAK
+  oUQDQgAEYtGFaHcu0Ig+pPL/jV5Flf3RLBYKNmylfByy+DcW4XfHMeS30d62JWjC
+  JzxoaTEREM61O6YwSiflQa37kU4eUw==
+  -----END EC PRIVATE KEY-----
+  """
+  ### openssl ec -in ecdsa.pem -pubout -out ecdsa.pub
+  @test_public_key_pem """
+  -----BEGIN PUBLIC KEY-----
+  MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEYtGFaHcu0Ig+pPL/jV5Flf3RLBYKNmyl
+  fByy+DcW4XfHMeS30d62JWjCJzxoaTEREM61O6YwSiflQa37kU4eUw==
+  -----END PUBLIC KEY-----
+  """
+  ### openssl ec -in ecdsa.pem -pubout -outform der | openssl sha1 -c
+  @test_fingerprint "82:33:e6:34:af:d4:0a:97:4f:f4:8a:82:69:0d:22:cb:d6:7e:02:be"
+
   test "generate" do
     pem = ECDSA.generate :secp256k1
-    [ec_params_der, ec_private_key_der] = :public_key.pem_decode pem
-    ec_params = :public_key.pem_entry_decode ec_params_der
-    ec_private_key = :public_key.pem_entry_decode ec_private_key_der
+    [ec_params_pem, ec_private_key_pem] = :public_key.pem_decode pem
+    ec_params = :public_key.pem_entry_decode ec_params_pem
+    ec_private_key = :public_key.pem_entry_decode ec_private_key_pem
     ec_point = {:ECPoint, ec_private_key |> elem(4)}
     ec_public_key = {ec_point, ec_params}
 
@@ -15,5 +36,12 @@ defmodule Keyz.ECDSATest do
     signature = :public_key.sign msg, :sha256, ec_private_key
     verify = :public_key.verify msg, :sha256, signature, ec_public_key
     assert verify
+  end
+
+  test "public_key" do
+    public_key_pem = ECDSA.public_key @test_pem
+    public_key_pem_tidy = String.replace public_key_pem, "\n", ""
+    test_public_key_pem_tidy = String.replace @test_public_key_pem, "\n", ""
+    assert test_public_key_pem_tidy == public_key_pem_tidy
   end
 end
